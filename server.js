0d6320b891e4cd9ca12dbc04d683781a34e587ae
Rohit-Patekar1 <64318761+Rohit-Patekar1@users.noreply.github.com>
2020-11-06 18:43:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
-
+const path = require('path');
 
 connectDB();
 
-app.get('/', (req, res) => res.send('API Running'));
-const PORT = process.env.PORT || 5000;
+
+
 
 //Init Middleware
 
@@ -19,7 +19,17 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assests in production
 
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
 
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port${PORT}`));
